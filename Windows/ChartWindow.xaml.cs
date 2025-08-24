3097a179fe5768e0614fd4a5698147f8f6ad1637
karakayaumit <94406130+karakayaumit@users.noreply.github.com>
2025-08-25 00:10:54 +0300

    fix chart window build

--- Windows/ChartWindow.xaml.cs
+++ Windows/ChartWindow.xaml.cs
@@ -148,7 +148,7 @@ namespace BinanceUsdtTicker
     }};
 
     async function loadCandles(symbol, interval) {{
-        const url = "https://api.binance.com/api/v3/klines?symbol=" + symbol + "&interval=" + interval + "&limit=200";
+        const url = 'https://api.binance.com/api/v3/klines?symbol=' + symbol + '&interval=' + interval + '&limit=200';
         const data = await fetch(url).then(r => r.json());
         const candles = data.map(c => ({{ time: c[0] / 1000, open: parseFloat(c[1]), high: parseFloat(c[2]), low: parseFloat(c[3]), close: parseFloat(c[4]) }}));
         setCandles(candles);
