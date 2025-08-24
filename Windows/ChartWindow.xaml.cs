2a641395b7fb87ad8e99f248e5ce6b6d94973bfa
karakayaumit <94406130+karakayaumit@users.noreply.github.com>
2025-08-25 00:04:10 +0300

    Fix JavaScript object escaping in chart HTML

--- Windows/ChartWindow.xaml.cs
+++ Windows/ChartWindow.xaml.cs
@@ -150,7 +150,7 @@ namespace BinanceUsdtTicker
     async function loadCandles(symbol, interval) {{
         const url = "https://api.binance.com/api/v3/klines?symbol=" + symbol + "&interval=" + interval + "&limit=200";
         const data = await fetch(url).then(r => r.json());
-        const candles = data.map(c => ({ time: c[0] / 1000, open: parseFloat(c[1]), high: parseFloat(c[2]), low: parseFloat(c[3]), close: parseFloat(c[4]) }));
+        const candles = data.map(c => ({{ time: c[0] / 1000, open: parseFloat(c[1]), high: parseFloat(c[2]), low: parseFloat(c[3]), close: parseFloat(c[4]) }}));
         setCandles(candles);
     }}
     window.loadCandles = loadCandles;
