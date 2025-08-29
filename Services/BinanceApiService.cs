f8c7cb2c2da45f078c4ed353400e6c14c5b40972
karakayaumit <94406130+karakayaumit@users.noreply.github.com>
2025-08-30 01:01:39 +0300

    Fix missing model namespace

--- Services/BinanceApiService.cs
+++ Services/BinanceApiService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Text.Json.Serialization;
 using System.Globalization;
+using BinanceUsdtTicker.Models;
 
 
 namespace BinanceUsdtTicker
