e63839a20918ec36168fcae55f748c05ffceee02
Ümit Karakaya <Karakayaumit@outlook.com>
2025-09-06 22:14:10 +0300

    Revert "Guard Windows-only logging setup"
    This reverts commit 86de13f5f0d6370041e8120d4bddc80f75566aee.

--- ListingWatcherService/Program.cs
+++ ListingWatcherService/Program.cs
@@ -2,17 +2,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ListingWatcher;
-using System;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Logging.ClearProviders();
+builder.Services.AddWindowsService(o => o.ServiceName = "Binance Watcher");
 
-if (OperatingSystem.IsWindows())
-{
-    builder.Services.AddWindowsService(o => o.ServiceName = "Binance Watcher");
-    builder.Logging.AddEventLog(o => o.SourceName = "ListingWatcherService");
-}
+builder.Logging.ClearProviders();
+builder.Logging.AddEventLog(o => o.SourceName = "ListingWatcherService");
 
 builder.Services.AddHostedService<ListingWatcherService>();
 
