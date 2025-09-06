bfb4c12b94dc9e7146769fb85ef02c32151e4f54
karakayaumit <94406130+karakayaumit@users.noreply.github.com>
2025-09-06 16:36:18 +0300

    refactor: decouple listing service

--- ListingWatcherService/Program.cs
+++ ListingWatcherService/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BinanceUsdtTicker;
+using ListingWatcher;
 
 Host.CreateDefaultBuilder(args)
     .UseWindowsService()
